19ce872fddb07d1c852ac7acdd139bd196a288a1
Kaynguyen0203 <kaynguyen0203@gmail.com>
2024-10-26 19:06:08 +0100

    css change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import {
