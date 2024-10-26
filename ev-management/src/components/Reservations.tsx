c1c80cd0a80e9f0c3739f8dd79d04a4793454ff1
HenriR21k <106344477+HenriR21k@users.noreply.github.com>
2024-10-26 10:42:33 +0100

    -tidying up

--- ev-management/src/components/Reservations.tsx
+++
@@ -1,9 +0,0 @@
-const Reservations = () => {
-  return (
-    <div className="container-fluid">
-      <h1>Book your charging slots in advance</h1>
-    </div>
-  );
-};
-
-export default Reservations;
