bbe527308fad395ce1ce790522add95568894e37
Kaynguyen0203 <kaynguyen0203@gmail.com>
2024-11-12 15:22:35 +0000

    Using useLoad.js to fetch markers

--- frontend/src/components/management.tsx
+++ frontend/src/components/management.tsx
@@ -1,46 +1,42 @@
 import { useEffect, useState } from "react";
-import { APIProvider, ControlPosition, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
-import {CustomMapControl} from './googlemapcomponents/map-control';
+import {
+  APIProvider,
+  ControlPosition,
+  Map,
+  AdvancedMarker,
+} from "@vis.gl/react-google-maps";
+import { CustomMapControl } from "./googlemapcomponents/map-control";
 import MapHandler from "./googlemapcomponents/map-handler";
-import { db } from '../config/firebase';
-import useLoad from './api/useLoad';
-import { collection, getDocs, addDoc, GeoPoint } from 'firebase/firestore';
+import { db } from "../config/firebase";
+import useLoad from "./api/useLoad";
+import { collection, getDocs, addDoc, GeoPoint } from "firebase/firestore";
 import configuration from "../config/configuration";
 
-
 export default function Management() {
-
-  const endpoint = `/Locations`
-  const [locations, setLocations, loadingMessage, loadLocations] = useLoad(endpoint)
+  const endpoint = `/Locations`;
+  const [locations, setLocations, loadingMessage, loadLocations] =
+    useLoad(endpoint);
   const [selectedPlace, setSelectedPlace] = useState<null>(null);
   const [markers, setMarkers] = useState<any[]>([]);
+  const [selectedMarker, setSelectedMarker] = useState(null);
 
   useEffect(() => {
-    const fetchMarkers = async () => {
-      try {
-        const locationsCollection = collection(db, 'Locations');
-        const locationSnapshot = await getDocs(locationsCollection);
-        
-        const locationData = locationSnapshot.docs.map(doc => {
-          const evlocation = doc.data().evlocation;
-          
-          return {
-            lat: evlocation._lat,
-            lng: evlocation._long
-          };
-        });
-        
-        console.log("Fetched locations:", locationData);
-        setMarkers(locationData);
-        
-      } catch (error) {
-        console.error("Error fetching locations: ", error);
-      }
-    };
-
-    fetchMarkers();
+    loadLocations();
   }, []);
-  
+
+  useEffect(() => {
+    if (locations) {
+      const locationData = locations.map((location: any) => {
+        const evlocation = location.evlocation;
+        return {
+          lat: evlocation.latitude,
+          lng: evlocation.longitude,
+        };
+      });
+      setMarkers(locationData);
+    } else console.log(loadingMessage);
+  }, [locations]);
+
   const addMarker = async (place: any) => {
     if (place && place.geometry && place.geometry.location) {
       const newMarker = {
@@ -49,13 +45,12 @@ export default function Management() {
       };
 
       setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
-      let testevlocation = new GeoPoint(newMarker.lat, newMarker.lng)
-      console.log("test: "+ JSON.stringify(testevlocation))
+      let testevlocation = new GeoPoint(newMarker.lat, newMarker.lng);
+      console.log("test: " + JSON.stringify(testevlocation));
       try {
-        const locationsCollection = collection(db, 'Locations');
+        const locationsCollection = collection(db, "Locations");
         await addDoc(locationsCollection, {
           evlocation: new GeoPoint(newMarker.lat, newMarker.lng),
-          
         });
         console.log("New marker added to Firestore:", newMarker);
       } catch (error) {
@@ -66,29 +61,26 @@ export default function Management() {
 
   return (
     <APIProvider apiKey={configuration.API.API_KEY}>
-    <Map
-        style={{width: '100vw', height: '100vh'}}
+      <Map
+        style={{ width: "100vw", height: "100vh" }}
         defaultZoom={3}
-        defaultCenter={{lat: 22.54992, lng: 0}}
-        gestureHandling={'greedy'}
+        defaultCenter={{ lat: 22.54992, lng: 0 }}
+        gestureHandling={"greedy"}
         disableDefaultUI={true}
         mapId={configuration.API.MAP_KEY}
-        >
+      >
         {markers.map((marker, index) => (
           <AdvancedMarker key={index} position={marker} />
         ))}
       </Map>
-    <CustomMapControl
+      <CustomMapControl
         controlPosition={ControlPosition.TOP}
         onPlaceSelect={(place) => {
           setSelectedPlace(place);
           addMarker(place);
         }}
-        
       />
-    <MapHandler place={selectedPlace} />
+      <MapHandler place={selectedPlace} />
     </APIProvider>
   );
 }
-
-
